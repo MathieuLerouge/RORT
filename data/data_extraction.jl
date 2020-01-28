bd1b4b1cd3da5b9f1792447b7a643aee02a8c604
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2020-01-28 17:41:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- data/data_extraction.jl
+++ data/data_extraction.jl
@@ -2,29 +2,65 @@
 # file_name must be a string which contains the extension
 function extract_data(file_name)
 
-    # Variables
-    n = 0
-    m = 0
-    r = 0
-    g = 0
-    Q = 0
-
-    # Open the text file
+    # Open the text file and read lines
     f = open(file_name)
-
-    # Read lines
     lines = readlines(f)
-    for l in lines
-       println(l)
+
+    # Read first line, i.e general data about the problem
+    l = lines[1]
+    words = split(l)
+    n = parse(Int64, words[1])
+    m = parse(Int64, words[2])
+    r = parse(Float64, words[3])
+    g = parse(Float64, words[4])
+    Q = parse(Float64, words[5]) # or Int64?
+
+    # Read first block of lines, i.e data about clients
+    es = zeros(Int64, n)
+    ls = zeros(Int64, n)
+    I = Set{Int}()
+    F = Set{Int}()
+    for l in lines[2:(n + 1)]
+        words = split(l)
+        i = parse(Int64, words[1])
+        es = parse(Int64, words[2])
+        ls = parse(Int64, words[3])
+        type = parse(Int64, words[3])
+        if (type == 0)
+            push!(I, i)
+        else
+            push!(F, i)
+        end
+    end
+
+    # Read second block of lines, i.e data about the arcs
+    distances = Dict{Tuple{Int64,Int64},Int64}()
+    times = Dict{Tuple{Int64,Int64},Int64}()
+    for l in lines[(3+n):end]
+        words = split(l)
+        i = parse(Int64, words[1])
+        j = parse(Int64, words[2])
+        dij = parse(Int64, words[3])
+        tij = parse(Int64, words[4])
+        distances[(i, j)] = dij
+        times[(i, j)] = tij
+    end
+
+    # Create neighbors sets
+    neighbors_in = [Set{Int}() for _ in 1:n]
+    neighbors_out = [Set{Int}() for _ in 1:n]
+    for (i,j) in keys(distances)
+        push!(neighbors_in[j], i)
+        push!(neighbors_out[i], j)
     end
 
     # Close the text file
     close(f)
 
-    # Output
-    n, m, r, g, Q
+    # Output all data
+    #n, m, r, g, Q, es, ls, distances, times
+    n, r, g, Q, es, ls, I, F, neighbors_in, neighbors_out, distances, times
 
 end
 
-
-## extract_data("E_data.txt")
+print(extract_data("E_data.txt"))
