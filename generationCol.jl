04224d731e7e2ff9b18bb3f2a92a123b9a31dee3
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2020-02-27 23:10:04 +0100

    End column generation.

--- generationCol.jl
+++ generationCol.jl
@@ -1,154 +1,277 @@
+################
 ## LIBRAIRIES ##
+################
 
 using JuMP
 using GLPK
 
 
-## INCLUDE ##
 
-folder_path = "/Users/camillegrange/Documents/RORT/"
-#folder_path = "/Users/lerougemathieu/Documents/Courses/MPRO/RORT/Project/"
+#############
+## INCLUDE ##
+#############
+
+# Set folder_path
+#folder_path = "/Users/camillegrange/Documents/RORT/"
+folder_path = "/Users/lerougemathieu/Documents/Courses/MPRO/RORT/Project/"
+
+# Prepare data extraction
 data_folder = "data/"
 data_folder_path = string(folder_path, data_folder)
 include(string(data_folder_path, "data_extraction.jl"))
 
 
+
+##########
 ## DATA ##
+##########
 
-## neighbours_in[i] = liste des j tq existe (j,i)
-## neighbours_out[i] = liste des j tq existe (i,j)
-
+# Set file_name
 file_name = "E_data.txt"
 #file_name = "E_data_1.txt"
+
+# Extract data
 n_tot, m, r, g, Q, es, ls, I, F,
     neighbours_in, neighbours_out, distances, times =
     extract_data(string(data_folder_path, file_name))
-
 n = n_tot - 2
 
-# Pas de temps de services :
+# Complete data
+# Assumption: no time serive
 s = [0 for i in 1:n+2]
-# Pas de colis à livrer :
+# Assumption: no demand to deliver
 q = [0 for i in 1:n+2]
-# Capacité des voitures :
+# Assumption: infinite capacity of vehicules
 C = 100000
 
-function solve_slave(var_duales)
 
+
+################
+## SUBPROBLEM ##
+################
+
+function solve_subproblem(dual_values)
+
+    ## MODEL ##
+
+    # Define the model
     model = Model(with_optimizer(GLPK.Optimizer))
 
 
-    ## VARIABLES ##
+    ## DECISION VARIABLES ##
 
+    # Define the decision variables
     @variable(model, x[i in 1:n+1,j in 2:n+2], Bin)
     @variable(model, tau[i in 1:n+2] >= 0)
-    @variable(model, u[i in 1:n+2] >= 0)
+    # Assumption: no demand to deliver
+    #@variable(model, u[i in 1:n+2] >= 0)
     @variable(model, y[i in 1:n+2] >= 0)
 
 
     ## CONSTRAINTS ##
 
-    #@constraint(model, ct_2[i in F],
-    #    sum(x[i,j] for j in neighbours_out[i] if j!=1) <= 1)
-
     # Flow conservation (4)
-    @constraint(model, ct_3[j in 2:n+1],
+    @constraint(model, cst_4[j in 2:n+1],
         sum(x[j,i] for i in neighbours_out[j]) -
         sum(x[i,j] for i in neighbours_in[j]) == 0)
 
     # Time feasability when leaving customers and depot (5)
-    @constraint(model, ct_4[i in union(I, 1), j in 2:n+2; (i,j) in keys(distances)],
+    @constraint(model, cst_5[i in union(I, 1), j in 2:n+2; (i,j) in keys(distances)],
         tau[i]+(times[(i,j)] + s[i])*x[i,j] - ls[1]*(1-x[i,j]) <= tau[j])
 
     # Time feasability when leaving recharging stations (6)
-    @constraint(model, ct_5[i in F, j in 2:n+2; (i,j) in keys(distances)],
+    @constraint(model, cst_6[i in F, j in 2:n+2; (i,j) in keys(distances)],
         tau[i]+times[(i,j)]*x[i,j] + g*(Q-y[i]) - (ls[1] + g*Q)*(1-x[i,j])
         <= tau[j])
 
     # Time windows (7)
-    @constraint(model, ct_6[j in 1:n+2], es[j] <= tau[j])
-    @constraint(model, ct_7[j in 1:n+2], tau[j] <= ls[j])
+    @constraint(model, cst_7[j in 1:n+2], es[j] <= tau[j])
+    @constraint(model, cst_7bis[j in 1:n+2], tau[j] <= ls[j])
 
-    # Demand feasability (8)
-    @constraint(model, ct_8[i in 1:n+1, j in 2:n+2; (i,j) in keys(distances)],
-        u[j] <= u[i] - q[i]*x[i,j] + C*(1-x[i,j]))
+    # Demand feasability (8) and (9)
+    # Assumption: no demand to deliver
+    #@constraint(model, cst_8[i in 1:n+1, j in 2:n+2; (i,j) in keys(distances)],
+    #    u[j] <= u[i] - q[i]*x[i,j] + C*(1-x[i,j]))
+    #@constraint(model, cst_9, u[1] <= C)
 
-    # Departure capacity (9)
-    @constraint(model, ct_9, u[1] <= C)
-
-    # Battery feasability (10)
-    @constraint(model, ct_10[i in I, j in 2:n+2;
+    # Battery feasability (10) and (11)
+    @constraint(model, cst_10[i in I, j in 2:n+2;
         (i,j) in keys(distances)],
         y[j] <= y[i] - r*distances[(i,j)]*x[i,j] + Q*(1-x[i,j]))
-    @constraint(model, ct_11[i in union(F,1), j in 2:n+2;
+    @constraint(model, cst_11[i in union(F,1), j in 2:n+2;
         (i,j) in keys(distances)],
         y[j] <= Q - r*distances[(i,j)]*x[i,j])
 
 
     ## OBJECTIVE ##
 
+    # Define the objective function
     @objective(model, Min,
         sum(distances[(i,j)]*x[i,j] for (i,j) in keys(distances))
-        - sum(var_duales[i]*x[i,j] for i in I for j in 2:n+2 if (i,j) in keys(distances)))
+        - sum(dual_values[i]*x[i,j] for i in I for j in 2:n+2 if (i,j) in keys(distances)))
+
 
     ## SOLVING ##
 
+    # Run the solving process
+    println("Start to run model of the sub-problem")
     optimize!(model)
+    println("End of the run.")
 
 
     ## RESULTS ##
 
-    xs_star = value.(x)
-    red_cost = objective_value(model)
-    println("Reduced cost: ", red_cost)
-    xs_star, red_cost
+    # Get solutions and values of the objective
+    x_star = value.(x)
+    reduced_cost = objective_value(model)
+
+    # Display
+    println("Reduced cost: ", reduced_cost)
+    println("")
+
+    # Output
+    x_star, reduced_cost
 
 end
 
 
 
+####################
+## MASTER PROBLEM ##
+####################
 
 function solve_master(routes)
 
-    model = Model(with_optimizer(GLPK.Optimizer))
+    ## GLOBAL VARIABLES ##
+
+    # Get the number of routes
     K = length(routes)
 
-    ## VARIABLES ##
+
+    ## MODEL ##
+
+    # Define the model
+    model = Model(with_optimizer(GLPK.Optimizer))
+
+
+    ## DECISION VARIABLES ##
+
+    # Define the decision variables
     @variable(model, delta[i in 1:K], Bin)
+
 
     ## CONSTRAINTS ##
 
-    @constraint(model, ct[i in I],
+    # Delivery constraint
+    @constraint(model, cst[i in I],
         sum( sum(routes[k][i,j]
             for j in 2:n+2 if (i,j) in keys(distances))*delta[k]
         for k in 1:K) == 1)
 
+
     ## OBJECTIVE ##
 
+    # Define the objective function
     @objective(model, Min,
         sum( sum(routes[k][i,j]*distances[(i,j)]
             for i in 1:n+1 for j in 2:n+2 if (i,j) in keys(distances))*delta[k]
         for k in 1:K))
 
+
     ## SOLVING ##
 
+    # Run the solving process
+    println("Start to run model of the master problem")
     optimize!(model)
+    println("End of the run.")
 
 
     ## RESULTS ##
 
+    # Get solutions and values of the objective
     total_distance = objective_value(model)
     delta_star = value.(delta)
-    nb_routes = sum(delta_star)
-    dual_values = getRowDualGLPK(model)
-    println("Number of routes: ", nb_routes)
-    println("Total distance: ", total_distance)
-    total_distance, nb_routes, dual_values
+    nb_routes = Int(sum(delta_star))
+
+    # Display
+    println("Current number of routes: ", nb_routes)
+    println("Current total distance: ", total_distance)
+    println("")
+
+    # Output
+    total_distance, nb_routes
+
+end
+
+
+function solve_relaxed_master(routes)
+
+    ## GLOBAL VARIABLES ##
+
+    # Get the number of routes
+    K = length(routes)
+
+
+    ## MODEL ##
+
+    # Define the model
+    model = Model(with_optimizer(GLPK.Optimizer))
+
+
+    ## DECISION VARIABLES ##
+
+    # Define the decision variables
+    @variable(model, delta[i in 1:K] >= 0)
+
+
+    ## CONSTRAINTS ##
+
+    # Delivery constraint
+    @constraint(model, cst_bin[i in K], delta[i] <= 1)
+    @constraint(model, cst[i in I],
+        sum( sum(routes[k][i,j]
+            for j in 2:n+2 if (i,j) in keys(distances))*delta[k]
+        for k in 1:K) == 1)
+
+
+    ## OBJECTIVE ##
+
+    # Define the objective function
+    @objective(model, Min,
+        sum( sum(routes[k][i,j]*distances[(i,j)]
+            for i in 1:n+1 for j in 2:n+2 if (i,j) in keys(distances))*delta[k]
+        for k in 1:K))
+
+
+    ## SOLVING ##
+
+    # Run the solving process
+    println("Start to run model of the relaxed master problem")
+    optimize!(model)
+    println("End of the run. \n")
+
+
+    ## RESULTS ##
+
+    # Get values of duals variables
+    dual_values = Dict{Int64,Float64}()
+    for i in I
+        dual_values[i] = dual(cst[i])
+    end
+
+    # Output
+    dual_values
 
 end
 
 
 
+######################################
+## INITIALIZATION OF MASTER PROBLEM ##
+######################################
+
+# Function which adds arcs (0, i) and (i, n+1)
+# with infinite distances and times, if needed
 function fill_graph(n_tot, m, r, g, Q, es, ls, I, F,
     neighbours_in, neighbours_out, distances, times)
     infinite = 10000
@@ -158,66 +281,61 @@ function fill_graph(n_tot, m, r, g, Q, es, ls, I, F,
         if((1,i) in keys(distances))
             continue
         else
-            distances[(1,i)] = infinite
-            times[(1,i)] = infinite
+            distances[(1, i)] = infinite
+            times[(1, i)] = infinite
         end
         if((i, n+2) in keys(distances))
             continue
         else
             distances[(i, n+2)] = infinite
-            times[(i,n+2)] = infinite
+            times[(i, n+2)] = infinite
         end
     end
 end
 
-### DICTIONNAIRE DES CHIS ? car par complet
-function routes_init()
+
+function initialize_routes()
     routes = []
     for i in I
         route_i = zeros(Int64, n+2, n+2)
-        route_i[1,i] = 1
-        route_i[i,n+2] = 1
-        append!(routes, route_i)
+        route_i[1, i] = 1
+        route_i[i, n+2] = 1
+        push!(routes, route_i)
     end
     routes
 end
 
-function gen_col(routes_ini)
-
-    red_cost = -1
-    routes = deepcopy(routes_ini)
-
-    while(red_cost < 0)
-
-        total_distance, nb_routes, dual_values = solve_master(routes)
-        new_route, red_cost = solve_slave(dual_values)
-        append!(routes, new_route)
-    end
-
-    total_distance, nb_routes
-
-end
 
 
-#####################"
-#       MAIN        #
-###################"#
+#######################
+## COLUMN GENERATION ##
+#######################
 
+# Add arcs for initialization if needed
 fill_graph(n_tot, m, r, g, Q, es, ls, I, F, neighbours_in, neighbours_out,
     distances, times)
 
-routes = routes_init()
-#println(neighbours_out[1][1])
+# Initialize routes for the first iteration if the master problem
+routes = initialize_routes()
 
-# Test
-println("Route ", routes)
-red_cost = -1
+# Initialize results with any values
+global_total_distance = -1
+global_nb_routes = -1
 
-while(red_cost < 0)
-    total_distance, nb_routes, dual_values = solve_master(routes)
-    new_route, red_cost = solve_slave(dual_values)
-    append!(routes, new_route)
+# Initialize reduced cost to a negative value (e.g. -1)
+global_reduced_cost = -1
+
+# Loop while reduced is negative
+while global_reduced_cost < 0
+    total_distance, nb_routes = solve_master(routes)
+    dual_values = solve_relaxed_master(routes)
+    new_route, reduced_cost = solve_subproblem(dual_values)
+    push!(routes, new_route)
+    global global_total_distance = total_distance
+    global global_nb_routes = nb_routes
+    global global_reduced_cost = reduced_cost
 end
 
-println("Number of routes: ", nb_routes)
-println("Total distance: ", total_distance)
+# Display results
+println("Number of routes: ", global_nb_routes)
+println("Total distance: ", global_total_distance)
