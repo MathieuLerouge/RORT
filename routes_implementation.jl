decf5ea12f8d6b37e8487c2ac29580b337d3dd80
Mathieu Lerouge <lerouge.mathieu91@gmail.com>
2020-03-20 19:19:12 +0100

    Add path to route conversion.

--- routes_implementation.jl
+++ routes_implementation.jl
@@ -14,6 +14,15 @@ function convert_arcs_into_path(route, n_tot)
     path
 end
 
+function convert_path_into_arcs(path, n_tot)
+    n = n_tot-2
+    route = zeros(Int64, n_tot, n_tot)
+    for i in 1:(length(path)-1)
+        route[path[i], path[i+1]] = 1
+    end
+    route
+end
+
 function display_route_as_path(route, n_tot)
     println(convert_arcs_into_path(route, n_tot))
 end
